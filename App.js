96d4486d549e3040cf9851fd754f4466e7805f48
Stephen Moore <judo2000@comcast.net>
2023-08-11 21:34:06 -0600

    Install react-native-paper and add Searchbar

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+//const isAndroid = Platform.OS === 'android';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
   return (
-    <View style={{ alignItems: 'center', flex: 1 }}>
-      <Text>Our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
+        <View style={styles.search}>
+          <Searchbar
+            placeholder="Search"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+          />
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  search: {
+    backgroundColor: "green",
+    padding: 16,
+  },
+  list: {
+    backgroundColor: "blue",
+    padding: 16,
+    flex: 1,
+  },
+});
