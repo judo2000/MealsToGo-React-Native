114a0400fabd1ce3c23079df9168268c3d39f1a3
Stephen Moore <judo2000@comcast.net>
2023-08-13 15:53:28 -0600

    Create reating component using react-vector-icons

---
+++ src/features/restaurants/components/rating.component.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components/native";
+import { FontAwesome } from "@expo/vector-icons";
+
+const StarContainer = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const FontAwesomeStar = styled(FontAwesome)`
+  font-size: 20px;
+  color: gold;
+`;
+
+export const Rating = ({ rating }) => {
+  console.log("rating", rating);
+  return (
+    <StarContainer>
+      <FontAwesomeStar
+        name={
+          rating >= 1 ? "star" : rating >= 0.5 ? "star-half-empty" : "star-o"
+        }
+      />
+      <FontAwesomeStar
+        name={
+          rating >= 2 ? "star" : rating >= 1.5 ? "star-half-empty" : "star-o"
+        }
+      />
+      <FontAwesomeStar
+        name={
+          rating >= 3 ? "star" : rating >= 2.5 ? "star-half-empty" : "star-o"
+        }
+      />
+      <FontAwesomeStar
+        name={
+          rating >= 4 ? "star" : rating >= 3.5 ? "star-half-empty" : "star-o"
+        }
+      />
+      <FontAwesomeStar
+        name={
+          rating >= 5 ? "star" : rating >= 4.5 ? "star-half-empty" : "star-o"
+        }
+      />
+    </StarContainer>
+  );
+};
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+//import { SvgXml } from "react-native-svg";
+import { Rating } from "./rating.component";
+
+import { FontAwesome } from "@expo/vector-icons";
+//import star from "../../../../assets/star";
 
 /*-----Styled Components-----*/
 const RestaurantCard = styled(Card)`
@@ -10,9 +15,21 @@ const RestaurantCardCover = styled(Card.Cover)`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
-const Title = styled.Text`
+
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`;
+
+const Title = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.title};
+  font-weight: ${(props) => props.theme.fontWeights.bold};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
 `;
 
 /*-----Styled Components-----*/
@@ -26,14 +43,22 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     ],
     address = "100 some random street",
     isOpenNow = true,
-    rating = 4,
+    rating = 3.6,
     isClosedTemporarily,
   } = restaurant;
+
+  //const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+
+        <Rating rating={rating} />
+
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
