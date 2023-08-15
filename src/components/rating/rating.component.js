e8a446d8974a4aa52d14c4ff620555725d76201c
Stephen Moore <judo2000@comcast.net>
2023-08-15 16:00:38 -0600

    Update star font size in rating component

--- src/components/rating/rating.component.js
+++ src/components/rating/rating.component.js
@@ -9,7 +9,7 @@ const StarContainer = styled.View`
 `;
 
 const FontAwesomeStar = styled(FontAwesome)`
-  font-size: 15px;
+  font-size: ${(props) => props.theme.sizes[1]};
   color: ${(props) => props.theme.colors.text.gold};
 `;
 
