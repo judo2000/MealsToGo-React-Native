31855f60bdae25fb483025445db5228b949a4f94
Stephen Moore <judo2000@comcast.net>
2023-09-02 16:35:25 -0600

    Move RestaurantInfoCard outside of ScrollView

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -13,8 +13,8 @@ export const RestaurantDetailScreen = ({ route }) => {
   const [drinksExpanded, setDrinksExpanded] = useState(false);
   return (
     <SafeArea>
+      <RestaurantInfoCard restaurant={restaurant} />
       <ScrollView>
-        <RestaurantInfoCard restaurant={restaurant} />
         <List.Accordion
           title="Breakfast"
           left={(props) => <List.Icon {...props} icon="bread-slice" />}
