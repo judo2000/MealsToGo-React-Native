7ad4533d9f4a32667f77d17a7b1f902c29ac1cc8
Stephen Moore <judo2000@comcast.net>
2023-09-15 22:07:27 -0600

    Use AsyncStorage to set and display user photo

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,21 +1,27 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { Camera } from "expo-camera";
 import styled from "styled-components/native";
 import { View, TouchableOpacity } from "react-native";
 import { Text } from "../../../components/typography/text.component";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
-export const CameraScreen = () => {
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+
+  const { user } = useContext(AuthenticationContext);
 
   const snap = async () => {
     if (cameraRef) {
       const photo = await cameraRef.current.takePictureAsync();
-      console.log(photo);
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
     }
   };
 
@@ -38,6 +44,7 @@ export const CameraScreen = () => {
       <ProfileCamera
         ref={(camera) => (cameraRef.current = camera)}
         type={Camera.Constants.Type.front}
+        ratio={"16:9"}
       ></ProfileCamera>
     </TouchableOpacity>
   );
